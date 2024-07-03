edf5db59a9f0632d126a3e00af6884beee632bf0
Murali Krishna A <muralivijayrahul@gmail.com>
2024-07-03 20:14:20 +0530

    Configured the initial setup and Added api addnewproduct and configured

---
+++ src/main/java/com/youtube/ecommerce/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.youtube.ecommerce.service;
+
+import com.youtube.ecommerce.dao.ProductDao;
+import com.youtube.ecommerce.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    public ProductDao productDao;
+    public Product addNewProduct(Product product) {
+        return productDao.save(product);
+    }
+
+
+    public List<Product> getAllProducts() {
+      return (List<Product>)productDao.findAll();
+    }
+}
